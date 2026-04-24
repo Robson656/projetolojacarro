dd1e295e32f58864df311ae995b91aff7a95b06b
Robson656 <suairma1598@gmail.com>
2026-04-24 16:37:19 -0300

    html

--- script/script.js
+++ script/script.js
@@ -2,43 +2,47 @@ const { createElement } = require("react");
 
 function salvarCarros(event) {
 //impedir de recarregar a página
-event.preventDefault();
+    event.preventDefault();
 
-let titulo = document.getElementById("title");
-let preco = document.getElementById("preco");
-let marca = document.getElementById("marca");
-let modelo = document.getElementById("modelo");
+    let titulo = document.getElementById("title").value;
+    let preco = document.getElementById("preco").value;
+    let marca = document.getElementById("marca").value;
+    let modelo = document.getElementById("modelo").value;
 
-let cambioSelecionado = document.querySelector('input[name="marcha"]:checked');
-let cambio = cambioSelecionado ? cambioSelecionado.id : "Não informado";
+    let cambioSelecionado = document.querySelector('input[name="marcha"]:checked');
 
-let lista = document.getElementById('listarCarros');
+    let cambio = cambioSelecionado ? cambioSelecionado.id : "Não informado";
 
-let card = document.createElement("div")
-
-card.style.border = "1px solid #ccc";
-card.style.padding = "10px";
-card.style.marginBottom = "10px";
-
-card.innerHTML = '
-<h3>${titulo}</h3>
-<p><strong>Preço:</strong> R$ ${preco}</p>
-<p><strong>Marca:</strong> ${marca}</p>
-<p><strong>Modelo:</strong> ${Modelo}</p>
-<p><strong>Câmbio:</strong> R$ ${cambio}</p>
-';
-
-lista.appendChild(card);
-
-let carro = {
+    let carro = {
     titulo,
     preco,
     marca,
     modelo,
     cambio
-};
+    };
 
-let carros = JSON.parse(localStorage.getItem("carros")) || [];
-carros.push(carro);
-localStorage.setItem("carros", JSON.stringify(carros));
+    let carros = JSON.parse(localStorage.getItem("carros")) || [];
+    carros.push(carro);
+    localStorage.setItem("carros", JSON.stringify(carros));
+
+    adicionarNaTela(carro);
+
+    document.querySelector("form").reset();
+}
+
+function adicionarNaTela(carro){
+    let lista = document.getElementById('listarCarros');
+    let card = document.createElement('div')
+    card.classList.add('card');
+
+    card.innerHTML  = `
+        <h3>${carro.titulo}</h3>
+        <p><strong>Preço:</strong> R$ ${carro.preco}</p>
+        <p><strong>Marca:</strong> ${carro.marca}</p>
+        <p><strong>Modelo:</strong> ${carro.modelo}</p>
+        <p><strong>Câmbio:</strong> ${carro.cambio}</p>
+    `;
+
+    lista.appendChild(card);
+    
 }
